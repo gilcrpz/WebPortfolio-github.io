cbfe99ec717f7d5f85ec0650d576b8c468a18c29
Angel Cayabyab <cayabyabangel6363@gmail.com>
2026-04-23 00:13:40 +0800

    updated the index.html, style, and script
    Made new modifications in the background, and I also added the project section

--- script.js
+++ script.js
@@ -37,3 +37,24 @@ const observer = new IntersectionObserver(entries => {
 });
 
 elements.forEach(el => observer.observe(el));
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("🌻 Your message has been sent!");
+});
+
+const buttons = document.querySelectorAll('.filter-buttons button');
+const cards = document.querySelectorAll('.project-card');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const filter = button.getAttribute('data-filter');
+    cards.forEach(card => {
+      if (filter === 'all' || card.getAttribute('data-category') === filter) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+});
+
